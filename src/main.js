f2e5dee8ea26320ba14f499381920b175542890e
Nidhal BEN GHZAIEL <nidhal.bg@opkodelabs.com>
2018-10-02 16:37:24 +0200

    added Ws getPost

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import GetPost from './GetPost.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(GetPost)
 }).$mount('#app')
