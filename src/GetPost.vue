3146b019f727decbd74596be1be19f42dc1e89d0
Nidhal BEN GHZAIEL <nidhal.bg@opkodelabs.com>
2018-10-04 12:45:59 +0200

    Updated axios to async methode

--- src/GetPost.vue
+++ src/GetPost.vue
@@ -6,11 +6,11 @@
     </li>
   </ul>
 
-  <!--<ul v-if="errors && errors.length">
-    <li v-for="error of errors">
+  <ul v-else-if="errors && errors.length">
+    <li v-for="(error, index) in errors" :key="index">
       {{error.message}}
     </li>
-  </ul>-->
+  </ul>
   
 
 </template>
@@ -22,29 +22,31 @@ export default {
   data() {
     return {
       posts: [],
-      errors: []
+      errors: ['yo', 'hi']
     }
   },
-
+  mounted () {
+    console.dir(this.errors);
+  },
   // Fetches posts when the component is created.
-  created() {
-    axios.get(`http://jsonplaceholder.typicode.com/posts`)
-    .then(response => {
-      // JSON responses are automatically parsed.
-      this.posts = response.data
-    })
-    .catch(e => {
-      this.errors.push(e)
-    })
+  async created() {
+    // axios.get(`http://jsonplaceholder.typicode.com/posts`)
+    // .then(response => {
+    //   // JSON responses are automatically parsed.
+    //   this.posts = response.data
+    // })
+    // .catch(e => {
+    //   this.errors.push(e)
+    // })
 
     // async / await version (created() becomes async created())
     //
-    // try {
-    //   const response = await axios.get(`http://jsonplaceholder.typicode.com/posts`)
-    //   this.posts = response.data
-    // } catch (e) {
-    //   this.errors.push(e)
-    // }
+    try {
+      const response = await axios.get(`http://jsonplaceholder.typicode.com/posts`)
+      this.posts = response.data
+    } catch (e) {
+      this.errors.push(e)
+    }
   }
 }
 </script>
